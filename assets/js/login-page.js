44ea02ba7a3b842f8ec998444038342ed58cd220
Gideon Tigelaar <contact@gideontigelaar.com>
2024-05-31 16:13:33 +0200

    Add db connection and login functionality

--- assets/js/login-page.js
+++ assets/js/login-page.js
@@ -5,3 +5,48 @@ function toggleLoginForm() {
     loginForm.classList.toggle('d-none');
     registerForm.classList.toggle('d-none');
 }
+
+function loginUser() {
+    var username = document.getElementById('login-username').value;
+    var password = document.getElementById('login-password').value;
+    setButtonLoadingState(['login-button'], true, true);
+
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', '../../queries/login-user.php', true);
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4) {
+            setButtonLoadingState(['login-button'], false, false);
+            if (xhr.status === 200) {
+                location.reload();
+            } else {
+                location.reload();
+            }
+        }
+    }
+    xhr.send('username=' + username + '&password=' + password);
+}
+
+function registerUser() {
+    var username = document.getElementById('register-username').value;
+    var email = document.getElementById('register-email').value;
+    var password = document.getElementById('register-password').value;
+    var passwordConfirm = document.getElementById('register-password-confirm').value;
+    var inviteCode = document.getElementById('register-invite-code').value;
+    setButtonLoadingState(['register-button'], true, true);
+
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', '../../queries/register-user.php', true);
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4) {
+            setButtonLoadingState(['register-button'], false, false);
+            if (xhr.status === 200) {
+                location.reload();
+            } else {
+                location.reload();
+            }
+        }
+    }
+    xhr.send('username=' + username + '&email=' + email + '&password=' + password + '&passwordConfirm=' + passwordConfirm + '&inviteCode=' + inviteCode);
+}

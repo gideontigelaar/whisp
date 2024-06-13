3c2bfed0b3b55c682bef6a063578165dff1a7d1c
Gideon Tigelaar <gidtigelaar@gmail.com>
2024-06-13 22:18:57 +0200

    Improve login screen and overall performance

--- queries/validate-session.php
+++ queries/validate-session.php
@@ -1,6 +1,19 @@
 <?php
 session_start();
-require_once $_SERVER['DOCUMENT_ROOT'] . "/queries/pdo-connect.php";
+try {
+    $creds = json_decode(file_get_contents($_SERVER['DOCUMENT_ROOT'] . "/creds.json"), true);
+    $dbhost = $creds['dbhost'];
+    $dbname = $creds['dbname'];
+    $dbuser = $creds['dbuser'];
+    $dbpass = $creds['dbpass'];
+
+    $pdo = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    http_response_code(500);
+    echo "An error occurred. Please try again later.";
+    exit();
+}
 
 function validateSession($pdo, $sessionToken)
 {
